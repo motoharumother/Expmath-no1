1103c2af334f11a63f0d32f27851ad505fc87c26
MotoharuKanou <motochanworld.gt@icloud.com>
2019-06-13 17:58:12 +0900

    report 2

--- report 2/fourier.py
+++ report 2/fourier.py
@@ -7,7 +7,11 @@ x = np.arange(-pi, pi, pi/1000)
 
 #関数定義
 def originalfunc(x):
-    return np.cos(4*x) + x
+    if np.sin(x)>0:
+        return 1
+    else:
+        return -1
+    
 #係数
 def ak(k):
     tmpa = integrate.quad(lambda x, c : (1/pi)*originalfunc(x)*np.cos(x*c), -pi, pi, args=k)[0]
@@ -33,7 +37,10 @@ y = np.zeros((len(nlist), len(x)))
 a_zero = integrate.quad(lambda x: (1/pi)*originalfunc(x), -pi, pi)[0]/2
 
 plt.figure(figsize=(10,7))
-plt.plot(x, originalfunc(x), label='cos(x) + x', c='r')
+origin_y = np.zeros(len(x))
+for i, _x in enumerate(x):
+    origin_y[i] = originalfunc(x[i])
+plt.plot(x, origin_y, label='f(x)', c='r')
 
 for m, n in enumerate(nlist):
     a = np.zeros((n))
