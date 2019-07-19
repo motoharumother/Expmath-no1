2d91132b68bb0a6dba22f59db5de6edb8087efae
MotoharuKanou <motochanworld.gt@icloud.com>
2019-07-19 21:48:00 +0900

    7/19

--- 7:28report /question3.py
+++ 7:28report /question3.py
@@ -1,17 +1,17 @@
 import numpy as np
+import sympy
 import matplotlib.pyplot as plt
-
+import time
 
 T = 10
-n = 100
-dt = T/n
-t = np.arange(0,10,dt)
+dt = 1e-2
+t = np.arange(0,T,dt)
 
 y = np.zeros((2,len(t)))
 
 y[0][0] = 2
-y[1][0] = 1
-
+y[1][0] = 1        
+    
 def f(x):
     X = x.copy()
     X[0] = x[0]*(x[1]-2)
@@ -37,15 +37,39 @@ def SympleticEuler(y, dt, t):
     for i in range(len(t)-1):
         y[0,i+1] = y[0,i] + dt*y[0,i]*(y[1,i]-2)
         y[1,i+1] = y[1,i]/(1-dt*(1-y[0,i+1]))
-        #A = np.linalg.inv(np.array([[1,-dt*y[0,i]],[dt*y[1,i], 1]]))
-        #y[:,i+1] = np.dot(A,np.array([(1-2*dt)*y[0,i],(1+dt)*y[1,i]]))
+    return y
+
+def ImEuler(y, dt, t):
+    u = sympy.Symbol('u')
+    v = sympy.Symbol('v')
+    for i in range(len(t)-1):
+        expr1 = u*(1-dt*(v-2))-y[0,i]
+        expr2 = v*(1-dt*(1-u))-y[1,i]
+        a = sympy.solve([expr1, expr2])
+        y[0,i+1] = a[-1][u]
+        y[1,i+1] = a[-1][v]
+    return y
+
+def MidpointRule(y, dt, t):
+    u = sympy.Symbol('u')
+    v = sympy.Symbol('v')
+    for i in range(len(t)-1):
+        expr1 = u-dt*(((y[0,i]+u)/2)*((y[1,i]+v)/2-2))-y[0,i]
+        expr2 = v-dt*(((y[1,i]+v)/2)*(1-(y[0,i]+u)/2))-y[1,i]
+        a = sympy.solve([expr1, expr2])
+        y[0,i+1] = a[-1][u]
+        y[1,i+1] = a[-1][v]
     return y
 
 
-
 #y = ExEuler(y, dt, t)
-#y = Runge_kutta(y, dt, t)
-y = SympleticEuler(y, dt, t)
+y = Runge_kutta(y, dt, t)
+#y = SympleticEuler(y, dt, t)
+#y = ImEuler(y, dt, t)
+#y = MidpointRule(y, dt, t)
+#y = (ExEuler(y, dt, t)+ImEuler(y, dt, t))/2
+
+
 plt.figure(figsize=(5,5))
 plt.plot(y[0], y[1])
 plt.scatter(y[0][0], y[1][0], label='t=0')
@@ -53,13 +77,20 @@ plt.scatter(y[0][-1], y[1][-1], label='t=10')
 plt.xlabel('u')
 plt.ylabel('v')
 plt.legend()
-#plt.title('Runge Kutta', fontdict={'fontsize':20})
+
+plt.title('Runge Kutta', fontdict={'fontsize':20})
 #plt.title('ExEuler', fontdict={'fontsize':20})
-plt.title('SympleticEuler', fontdict={'fontsize':20})
+#plt.title('SympleticEuler', fontdict={'fontsize':20})
+#plt.title('ImEuler', fontdict={'fontsize':20})
+#plt.title('MidpointRule', fontdict={'fontsize':20})
+#plt.title('Ex-Im-Euler', fontdict={'fontsize':20})
 
 #plt.savefig('Runge Kutta')
 #plt.savefig('ExEuler')
 #plt.savefig('SympleticEuler')
+#plt.savefig('ImEuler')
+#plt.savefig('MidpointRule')
+
 
 #
 plt.show()
